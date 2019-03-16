258708af2975a424cff9a2ab5c0298625f6af470
github username <github email address>
2019-03-16 12:15:23 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module BlogFlash
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.action_controller.permit_all_parameters = true
+    class Application < Rails::Application
   end
 end
